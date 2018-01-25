82ae8d10c5f9727bf37add1dae8f25663ec6148b
Greg Ferrell <gferrell20@gmail.com>
2018-01-24 21:59:42 -0400

    clean up fib problem

--- problems/ic_15_fib.py
+++ problems/ic_15_fib.py
@@ -5,90 +5,83 @@
 from math import pow, sqrt
 
 
-class Fib:
-    def __init__(self, logging: bool = False):
-        self.logging = logging
+def fib_1_recursive(n):
+    """
+    Solution: Brute force recursive solution.
+    Complexity:
+        Description: Number of computations can be represented as a binary
+        tree has height of n.
+        Time: O(2^n)
+    """
+    if n < 0:
+        raise ValueError('input must be a positive whole number')
 
-    def fib_1_recursive(self, n):
-        """
-        Solution: Brute force recursive solution.
-        Complexity:
-            Description: Number of computations can be represented as a binary
-            tree has height of n.
-            Time: O(2^n)
-        """
-        if n < 0:
-            raise ValueError('input must be a positive whole number')
+    if n in [0, 1]:
+        return n
 
-        if n in [0, 1]:
-            return n
+    return fib_1_recursive(n - 2) + fib_1_recursive(n - 1)
 
-        return self.fib_1_recursive(n - 2) + self.fib_1_recursive(n - 1)
 
-    def fib_2_recursive_memoize(self, n, cache={}):
-        """
-        Solution: Recursive solution that memorizes previously computed
-        values by storing them in memory.
-        Complexity:
-            Time: O(n)
-            Space: O(n)
-        """
-        if n < 0:
-            raise ValueError('input must be a positive whole number')
+def fib_2_recursive_memoize(n, cache={}):
+    """
+    Solution: Recursive solution that memorizes previously computed
+    values by storing them in memory.
+    Complexity:
+        Time: O(n)
+        Space: O(n)
+    """
+    if n < 0:
+        raise ValueError('input must be a positive whole number')
 
-        if n in [0, 1]:
-            return n
+    if n in [0, 1]:
+        return n
 
-        func_name = self.fib_2_recursive_memoize.__name__
-        if n in cache:
-            if self.logging:
-                print('{}: getting {} from cache'.format(func_name, n))
-            return cache[n]
+    if n in cache:
+        return cache[n]
 
-        if self.logging:
-            print('{}: computing fib for {}'.format(func_name, n))
-        val = self.fib_2_recursive_memoize(n - 2,
-                                           cache) + self.fib_2_recursive_memoize(
-            n - 1, cache)
-        cache[n] = val
-        return val
+    val = fib_2_recursive_memoize(n - 2, cache) + fib_2_recursive_memoize(n - 1,
+                                                                          cache)
+    cache[n] = val
+    return val
 
-    def fib_3_iterative(self, n):
-        """
-        Solution: Iterative solution.
-        Complexity:
-            Time: O(n)
-            Space: O(1)
-        """
-        if n < 0:
-            raise ValueError('input must be a positive whole number')
 
-        if n in [0, 1]:
-            return n
+def fib_3_iterative(n):
+    """
+    Solution: Iterative solution.
+    Complexity:
+        Time: O(n)
+        Space: O(1)
+    """
+    if n < 0:
+        raise ValueError('input must be a positive whole number')
 
-        p1 = 0
-        p2 = 1
-        return_val = 0
+    if n in [0, 1]:
+        return n
 
-        for _ in range(n - 1):
-            return_val = p1 + p2
-            p1 = p2
-            p2 = return_val
+    p1 = 0
+    p2 = 1
+    return_val = 0
 
-        return return_val
+    for _ in range(n - 1):
+        return_val = p1 + p2
+        p1 = p2
+        p2 = return_val
 
-    def fib_4_binets_formula(self, n):
-        """
-        Solution (cheat): Use Binet's formula.
-        Sometimes math just wins.
-        Complexity:
-            Time: O(1)
-            Space: O(1)
-        """
-        if n < 0:
-            raise ValueError('input must be a positive whole number')
+    return return_val
 
-        v1 = 1 / sqrt(5)
-        v2 = pow((1 + sqrt(5)) / 2, n)
-        v3 = pow((1 - sqrt(5)) / 2, n)
-        return int(v1 * (v2 - v3))
+
+def fib_4_binets_formula(n):
+    """
+    Solution (cheat): Use Binet's formula.
+    Sometimes math just wins.
+    Complexity:
+        Time: O(1)
+        Space: O(1)
+    """
+    if n < 0:
+        raise ValueError('input must be a positive whole number')
+
+    v1 = 1 / sqrt(5)
+    v2 = pow((1 + sqrt(5)) / 2, n)
+    v3 = pow((1 - sqrt(5)) / 2, n)
+    return int(v1 * (v2 - v3))
--- problems/test_ic_15_fib.py
+++ problems/test_ic_15_fib.py
@@ -1,7 +1,9 @@
+from timeit import default_timer
+
 import pytest
 
-from ic_15_fib import Fib
-from timeit import default_timer
+from ic_15_fib import fib_1_recursive, fib_2_recursive_memoize, fib_3_iterative, \
+    fib_4_binets_formula
 
 
 def assert_values(f):
@@ -21,50 +23,44 @@ def assert_values(f):
 
 
 def test_fib_recursive():
-    f = Fib()
-    assert_values(f.fib_1_recursive)
+    assert_values(fib_1_recursive)
 
 
 def test_fib_recursive_memoize():
-    f = Fib()
-    assert_values(f.fib_2_recursive_memoize)
+    assert_values(fib_2_recursive_memoize)
 
 
 def test_fib_iterative():
-    f = Fib()
-    assert_values(f.fib_3_iterative)
+    assert_values(fib_3_iterative)
 
 
 def test_fib_binets_formula():
-    f = Fib()
-    assert_values(f.fib_4_binets_formula)
+    assert_values(fib_4_binets_formula)
 
 
 def test_speed_of_algorithms():
-    f = Fib()
-
     start = default_timer()
-    f.fib_1_recursive(25)
+    fib_1_recursive(25)
     end = default_timer()
     length_recursive = end - start
 
     start = default_timer()
-    f.fib_2_recursive_memoize(25)
+    fib_2_recursive_memoize(25)
     end = default_timer()
     length_recursive_memoize = end - start
 
     start = default_timer()
-    f.fib_3_iterative(25)
+    fib_3_iterative(25)
     end = default_timer()
     length_iterative_short = end - start
 
     start = default_timer()
-    f.fib_3_iterative(500)
+    fib_3_iterative(500)
     end = default_timer()
     length_iterative = end - start
 
     start = default_timer()
-    f.fib_4_binets_formula(500)
+    fib_4_binets_formula(500)
     end = default_timer()
     length_binets = end - start
 
