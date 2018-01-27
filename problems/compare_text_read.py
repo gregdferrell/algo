c39d50646fbc9e64fa948fe2c5ea973bae3f6cd7
Greg Ferrell <gferrell20@gmail.com>
2018-01-27 15:26:18 -0400

    improve documentation, tests and code readability for compare_text

--- problems/compare_text_read.py
+++ problems/compare_text_read.py
@@ -5,15 +5,31 @@
 #
 # Algorithm should maximize the number of matches in sequential order.
 #
-# TODO: creating more permutations than needed when forking; fix this
-#       consider using tree data structure
-#       clean up this awful code in other ways
+# TODO: consider using dicts/tree instead of arrays
+#
+
+
+class MatchIndexMapping:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        return f"({self.key}, {self.value})"
+
+
+def print_all_results(all_results):
+    print("\nAll Results:")
+    for index, result in enumerate(all_results):
+        print(str(index) + ":")
+        for match_index_mapping in result:
+            print(match_index_mapping)
 
 
 def get_all_possible_mappings(orig, alt):
     """
-    For each item in the original list, calculates a list of indexes in the
-    altered list that match the item in the original list.
+    Creates a list of lists that contains the mappings of the items in orig
+    to all possible indexes from alt.
     :param orig: a list of items
     :param alt: a list of items
     :return: a list of lists, each element represents a list of indexes in the
@@ -29,45 +45,49 @@ def get_all_possible_mappings(orig, alt):
     return all_mappings
 
 
-def print_all_results(all_results):
-    print("\nAll Results:")
-    for index, result in enumerate(all_results):
-        print(str(index) + ":")
-        for match_index_mapping in result:
-            print(match_index_mapping)
+def compare_text(orig, alt, debug=False):
+    """
+    Gets the maximum length sequential intersection of data from org to alt.
+    This is represented in a list of indexes from orig to alt.
+    :param orig: the original data
+    :param alt: the altered data
+    :param debug: boolean indicating whether to print out debug info
+    :return: a list of MatchIndexMapping that contains a key (the index of
+    the value in the orig data) and a value (the index of the data mapped to)
+    in the alt data
+    """
 
-
-class MatchIndexMapping:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    def __str__(self):
-        return f"({self.key}, {self.value})"
-
-
-def sequential_compare(orig, alt, debug=False):
+    # Convert lists of data to list of lists mapping indexes from orig to
+    # indexes in alt.
     all_mappings = get_all_possible_mappings(orig, alt)
     if debug:
         print("\nAll Mappings:\n" + str(all_mappings))
+
     results = []
     for i, index_list in enumerate(all_mappings):
-        # If our index list is empty, proceed to next match_index_mapping array
+        # If our index list is empty, then the data point in orig that
+        # this index represents doesn't map to any data point in alt, so
+        # proceed to next match_index_mapping array
         if not index_list:
             continue
 
-        # If there are no root nodes, create one from this index list
+        # If there are no results yet, create one from the first item in
+        # this index list
         if not results:
             results.append([MatchIndexMapping(i, index_list[0])])
             continue
 
-        # Append lowest possible index from index_list to each result.
+        # For each index list, append the lowest possible index from the list
+        # to each result.
         #
-        # This will either result in either:
-        # 1. Simply appending it to the end of a result or
-        # 2. Inserting it into the middle in which case we'll fork the
-        # result into a copy and insert it there, blowing away the remaining
-        # elements.
+        # This will result in either:
+        # 1. Simply appending the index to the end of a result or
+        # 2. Inserting the index into the middle of a result. In this case
+        # we'll fork the result creating a copy and insert it into the copy
+        # which also blows away the indexes after it. This needs to be done
+        # because there is no guarantee that simply appending an index on the
+        # end of a result will achieve our goal of getting the longest
+        # possible sequential intersection.
         forked_results = []
         for result in results:
             next_result = False
@@ -97,12 +117,12 @@ def sequential_compare(orig, alt, debug=False):
             results.extend(forked_results)
 
         # If nothing in this index was added to an existing result, then
-        # create a new root node if no nodes exist whose initial value is less
-        # than the smallest value in the array
+        # create a new result if no results exist whose initial value is less
+        # than the smallest value in the index list
         for result in results:
             initial_value = result[0].value
-            first_value_from_array = index_list[0]
-            if initial_value <= first_value_from_array:
+            first_value_from_index_list = index_list[0]
+            if initial_value <= first_value_from_index_list:
                 break
         else:
             results.append([MatchIndexMapping(i, index_list[0])])
@@ -110,16 +130,8 @@ def sequential_compare(orig, alt, debug=False):
     if debug:
         print_all_results(results)
 
-    # Return longest result
-    index_longest = -1
-    size_longest = -1
-    for index_results, result in enumerate(results):
-        if len(result) > size_longest:
-            index_longest = index_results
-            size_longest = len(result)
-
-    return results[index_longest]
-
+    # Return the result of the longest length
+    return max(results, key=len)
 
 if __name__ == "__main__":
     l1 = [1, 2, 3, 4, 5]
@@ -127,7 +139,7 @@ if __name__ == "__main__":
 
     print("\nList 1: " + str(l1))
     print("\nList 2: " + str(l2))
-    result = sequential_compare(l1, l2, True)
+    test_result = compare_text(l1, l2, True)
     print("\nFinal Result:")
-    for match_index_mapping in result:
-        print(match_index_mapping)
+    for mim in test_result:
+        print(mim)
