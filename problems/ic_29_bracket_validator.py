5f1d38c918fbe2418347a125ba77a9b2d10b39d5
Greg Ferrell <gferrell20@gmail.com>
2018-03-14 10:16:25 -0300

    add problem for bracket validator

---
+++ problems/ic_29_bracket_validator.py
@@ -0,0 +1,29 @@
+#
+# Problem: Write an efficient function that tells us whether or not an input string's openers and
+# closers are properly nested.
+#
+
+
+def bracket_validator(text: str):
+    """
+    Solution: Iterate and push/pop brackets on stack.
+    Complexity:
+        Time: O(n)
+        Space: O(n)
+    """
+    if not text:
+        raise ValueError('Input string needs a value')
+
+    stack = []
+
+    for i, c in enumerate(text):
+        if c in ('{', '[', '('):
+            stack.append(c)
+        elif c in ('}', ']', ')'):
+            if not stack:
+                return False
+            c_popped = stack.pop()
+            if not (c_popped == '{' and c == '}' or c_popped == '[' and c == ']' or c_popped == '(' and c == ')'):
+                return False
+
+    return not stack
---
+++ problems/test_ic_29_bracket_validator.py
@@ -0,0 +1,105 @@
+from ic_29_bracket_validator import bracket_validator
+
+
+def test_bracket_validator_paren():
+    text = "alert('hi');"
+    assert bracket_validator(text)
+
+
+def test_bracket_validator_curly():
+    text = "alert{'hi'};"
+    assert bracket_validator(text)
+
+
+def test_bracket_validator_box():
+    text = "alert['hi'];"
+    assert bracket_validator(text)
+
+
+def test_bracket_validator_paren_no_open():
+    text = "alert'hi');"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_curly_no_open():
+    text = "alert'hi'};"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_box_no_open():
+    text = "alert'hi'];"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_paren_no_close():
+    text = "alert('hi';"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_curly_no_close():
+    text = "alert{'hi';"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_box_no_close():
+    text = "alert['hi';"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_paren_wrong_close():
+    text = "alert('hi'];"
+    assert not bracket_validator(text)
+
+    text = "alert('hi'};"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_curly_wrong_close():
+    text = "alert{'hi'];"
+    assert not bracket_validator(text)
+
+    text = "alert{'hi');"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_box_wrong_close():
+    text = "alert['hi');"
+    assert not bracket_validator(text)
+
+    text = "alert['hi'};"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_multiple():
+    text = "alert('hi');(){}[]abc(abc)abc{abc}abc[abc]abc"
+    assert bracket_validator(text)
+
+
+def test_bracket_validator_nested():
+    text = "([abcde(fg{h[]i[]j}klmnopqrst)uvwx]{y}z)"
+    assert bracket_validator(text)
+
+
+def test_bracket_validator_nested_bad_1():
+    text = "([abcde(fg{h[]i[]j}klmnopqrst)uvwx]{y}z"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_nested_bad_2():
+    text = "([abcde(fg{h[]i]j}klmnopqrst)uvwx]{y}z)"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_nested_bad_3():
+    text = "([abcde(fg{h[]i[]j}klmnopqrstuvwx]{y}z)"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_nested_bad_4():
+    text = "(abcde(fg{h[]i[]j}klmnopqrst)uvwx]{y}z)"
+    assert not bracket_validator(text)
+
+
+def test_bracket_validator_nested_bad_5():
+    text = "([abcde(fgh[]i[]j}klmnopqrst)uvwx]{y}z)"
+    assert not bracket_validator(text)
