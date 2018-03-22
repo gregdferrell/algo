53136202044725347e6c05b4215e7a1eaafa5988
Greg Ferrell <gferrell20@gmail.com>
2018-03-22 09:00:00 -0300

    feat: Add util class for binary tree node and use tabs instead of spaces

--- problems/problem_solve_util.py
+++ problems/problem_solve_util.py
@@ -5,43 +5,59 @@ from math import floor
 
 
 def binary_search(target, nums):
-    """
-    Checks to see if a given number appears in a sorted list of numbers.
-    :param target: The number to look for
-    :param nums: A sorted (ascending) list of numbers
-    :return: Boolean indicating if the target was found
-    """
-    first_index = 0
-    last_index = len(nums) - 1
+	"""
+	Checks to see if a given number appears in a sorted list of numbers.
+	:param target: The number to look for
+	:param nums: A sorted (ascending) list of numbers
+	:return: Boolean indicating if the target was found
+	"""
+	first_index = 0
+	last_index = len(nums) - 1
 
-    # If the target value isn't between the values at the beginning and end
-    # of our list, return False immediately
-    if not (nums[0] <= target <= nums[-1]):
-        return False
+	# If the target value isn't between the values at the beginning and end
+	# of our list, return False immediately
+	if not (nums[0] <= target <= nums[-1]):
+		return False
 
-    while True:
-        # Calculate our target index -the middle point between the first
-        # and last indexes that we're tracking
-        target_index = (floor((last_index - first_index) / 2)) + first_index
-        if target == nums[target_index]:
-            return True
-        elif first_index == last_index:
-            return False
-        elif target < nums[target_index]:
-            last_index = target_index
-        elif target > nums[target_index]:
-            first_index = target_index + 1
-
-    return False
+	while True:
+		# Calculate our target index -the middle point between the first
+		# and last indexes that we're tracking
+		target_index = (floor((last_index - first_index) / 2)) + first_index
+		if target == nums[target_index]:
+			return True
+		elif first_index == last_index:
+			return False
+		elif target < nums[target_index]:
+			last_index = target_index
+		elif target > nums[target_index]:
+			first_index = target_index + 1
 
 
 class LinkedListNode:
-    """
-    Linked List Node
-    """
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+	"""
+	Linked List Node
+	"""
 
-    def __repr__(self):
-        return self.value
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+
+	def __repr__(self):
+		return self.value
+
+
+class BinaryTreeNode:
+	"""
+	Binary Tree Node
+	"""
+
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+	def insert_left(self, value):
+		self.left = BinaryTreeNode(value)
+
+	def insert_right(self, value):
+		self.right = BinaryTreeNode(value)
