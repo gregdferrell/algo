aacb5dca4ffdbf3e0aa8592c847764824ff0f35d
Greg Ferrell <gferrell20@gmail.com>
2018-03-31 22:52:21 -0300

    update graph coloring algorithm

--- problems/ic_45_graph_coloring.py
+++ problems/ic_45_graph_coloring.py
@@ -3,11 +3,41 @@
 #
 
 
-def color_graph(graph, colors):
+def color_graph_first_available(graph, colors):
 	"""
-	Solution:
-	Complexity:
-		Time: O()
-		Space: O()
+	Solution: For each graph node, assign to it the first color not in the list of neighbor colors.
+	Complexity: (where n is # nodes, d is max degrees)
+		Time: O(n * (d + d+1 + 1)) -> O(n * d)
+		Space: O(g) -> size of graph
 	"""
-	pass
+	# For every node
+	for node in graph:
+		if node in node.neighbors:
+			raise ValueError('cannot color graph with node loops')
+
+		# Look at each neighbor and create a set of their colors
+		neighbor_colors = set([neighbor.color for neighbor in node.neighbors if neighbor.color])
+		# Look at each color and create a set of available colors not in the neighbor colors set
+		available_colors = [color for color in colors if color not in neighbor_colors]
+		# Take the first available
+		node.color = available_colors[0]
+
+
+def color_graph_first_available_slightly_faster(graph, colors):
+	"""
+	Solution: For each graph node, assign to it the first available color not used by one of its neighbors.
+	Complexity: (where n is # nodes, d is max degrees)
+		Time: O(n * d)
+		Space: O(g) -> size of graph
+	"""
+	for node in graph:
+		if node in node.neighbors:
+			raise ValueError('cannot color graph with node loops')
+
+		# Look at each neighbor and create a set of their colors
+		neighbor_colors = set([neighbor.color for neighbor in node.neighbors if neighbor.color])
+
+		for color in colors:
+			if color not in neighbor_colors:
+				node.color = color
+				break
--- problems/test_ic_45_graph_coloring.py
+++ problems/test_ic_45_graph_coloring.py
@@ -1,10 +1,17 @@
-from ic_45_graph_coloring import color_graph
+import pytest
+
+from ic_45_graph_coloring import color_graph_first_available_slightly_faster
 from problem_solve_util import GraphNode
 
 colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange']
 
 
 def get_colors_for_graph(graph):
+	"""
+	Given a graph, returns a list of colors whose length is D + 1 where D = the max degree of the graph
+	:param graph: the graph to return a list of colors for
+	:return: the list of colors
+	"""
 	max_neighbors = 0
 	for graph_node in graph:
 		max_neighbors = max(max_neighbors, len(graph_node.neighbors))
@@ -14,13 +21,31 @@ def get_colors_for_graph(graph):
 
 
 def validate_graph_colors(graph):
+	"""
+	Validates that each graph node has a color distinct from the colors of its neighbors.
+	:param graph: the graph to examine
+	:return: boolean indicating whether the graph nodes have valid colors
+	"""
 	for graph_node in graph:
+		print(graph_node.color + ': ' + ', '.join(str(o.color) for o in graph_node.neighbors))
 		if graph_node.color in (o.color for o in graph_node.neighbors):
 			return False
 	return True
 
 
-def test_color_graph_1():
+def test_color_graph_1_node():
+	a = GraphNode('a')
+
+	graph = [a]
+
+	graph_colors = get_colors_for_graph(graph)
+
+	color_graph_first_available_slightly_faster(graph, graph_colors)
+
+	assert validate_graph_colors(graph)
+
+
+def test_color_graph_3_nodes():
 	a = GraphNode('a')
 	b = GraphNode('b')
 	c = GraphNode('c')
@@ -32,13 +57,100 @@ def test_color_graph_1():
 
 	graph = [a, b, c]
 
-	colors = get_colors_for_graph(graph)
+	graph_colors = get_colors_for_graph(graph)
 
-	# TODO impl algorithm instead of hard coding this example
-	color_graph(graph, colors)
-	a.color = 'red'
-	b.color = 'blue'
-	c.color = 'red'
+	color_graph_first_available_slightly_faster(graph, graph_colors)
 
-	# Validate graph colors
 	assert validate_graph_colors(graph)
+
+
+def test_color_graph_many_nodes():
+	a = GraphNode('a')
+	b = GraphNode('b')
+	c = GraphNode('c')
+	d = GraphNode('d')
+	e = GraphNode('e')
+
+	a.neighbors.add(b)
+	a.neighbors.add(c)
+	a.neighbors.add(d)
+	a.neighbors.add(e)
+	b.neighbors.add(a)
+	b.neighbors.add(c)
+	b.neighbors.add(d)
+	b.neighbors.add(e)
+	c.neighbors.add(a)
+	c.neighbors.add(b)
+	d.neighbors.add(a)
+	d.neighbors.add(b)
+	d.neighbors.add(e)
+	e.neighbors.add(a)
+	e.neighbors.add(b)
+	e.neighbors.add(d)
+
+	graph = [a, b, c, d, e]
+
+	graph_colors = get_colors_for_graph(graph)
+
+	color_graph_first_available_slightly_faster(graph, graph_colors)
+
+	assert validate_graph_colors(graph)
+
+
+def test_color_graph_small():
+	a = GraphNode('a')
+	b = GraphNode('b')
+	c = GraphNode('c')
+	d = GraphNode('d')
+
+	a.neighbors.add(b)
+	b.neighbors.add(a)
+	b.neighbors.add(c)
+	c.neighbors.add(b)
+	c.neighbors.add(d)
+	d.neighbors.add(c)
+
+	graph = [a, b, c, d]
+
+	graph_colors = get_colors_for_graph(graph)
+
+	color_graph_first_available_slightly_faster(graph, graph_colors)
+
+	assert validate_graph_colors(graph)
+
+
+def test_color_graph_isolated_nodes():
+	a = GraphNode('a')
+	b = GraphNode('b')
+	c = GraphNode('c')
+	d = GraphNode('d')
+	e = GraphNode('e')
+
+	a.neighbors.add(b)
+	b.neighbors.add(a)
+	c.neighbors.add(d)
+	d.neighbors.add(c)
+
+	graph = [a, b, c, d, e]
+
+	graph_colors = get_colors_for_graph(graph)
+
+	color_graph_first_available_slightly_faster(graph, graph_colors)
+
+	assert validate_graph_colors(graph)
+
+
+def test_color_graph_node_loops():
+	a = GraphNode('a')
+	b = GraphNode('b')
+
+	a.neighbors.add(b)
+	b.neighbors.add(a)
+	b.neighbors.add(b)
+
+	graph = [a, b]
+
+	graph_colors = get_colors_for_graph(graph)
+
+	with pytest.raises(ValueError) as e:
+		color_graph_first_available_slightly_faster(graph, graph_colors)
