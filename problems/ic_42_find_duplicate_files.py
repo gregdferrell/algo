57d6c12775018644ba6cfcedd51805c19b29c180
Greg Ferrell <gferrell20@gmail.com>
2018-04-16 12:49:02 -0300

    feat: Finish problem for finding file duplicates

--- problems/ic_42_find_duplicate_files.py
+++ problems/ic_42_find_duplicate_files.py
@@ -7,18 +7,60 @@
 # Assume each file was only duplicated once
 #
 
+import hashlib
 import os
+
+
+def chunk_reader(file, chunk_size=1024):
+	while True:
+		chunk = file.read(chunk_size)
+		if not chunk:
+			return
+		yield chunk
+
+
+def get_hash(filename, first_chunk_only=False):
+	# Use sha1 hashing algorithm
+	hashed_object = hashlib.sha1()
+	file_object = open(filename, 'rb')
+
+	# Feed it bytes
+	if first_chunk_only:
+		hashed_object.update(file_object.read(1024))
+	else:
+		for chunk in chunk_reader(file_object):
+			hashed_object.update(chunk)
+
+	# Get the digest
+	hashed = hashed_object.digest()
+
+	file_object.close()
+	return hashed
 
 
 def find_duplicate_files(base_dir):
 	"""
 	Solution: ???
 	Complexity:
-		Time: O(?)
-		Space: O(?)
+		Time: O(n) - The time it takes to hash the whole file (or the first chunk)
+		Space: O(n) - The hash (plus the list of duplicates)
 	"""
+	duplicates = []
+	hashes = {}
 	for root, directories, filenames in os.walk(base_dir):
 		for filename in filenames:
 			file_path = os.path.join(root, filename)
-			print(os.path.join(root, filename))
-			print(os.path.getmtime(file_path))
+
+			# Get file hash
+			hash = get_hash(file_path)
+
+			# Compare with dict
+			if not hashes.get(hash):
+				hashes[hash] = file_path
+			else:
+				if os.path.getmtime(file_path) > os.path.getmtime(hashes.get(hash)):
+					duplicates.append((file_path, hashes.get(hash)))
+				else:
+					duplicates.append((hashes.get(hash), file_path))
+				del hashes[hash]
+	return duplicates
--- problems/test_ic_42_find_duplicate_files.py
+++ problems/test_ic_42_find_duplicate_files.py
@@ -1,6 +1,10 @@
+import os
+
 from ic_42_find_duplicate_files import find_duplicate_files
 
 
 def test_find_duplicate_files():
-	base_dir = '../data'
-	response = find_duplicate_files(base_dir)
+	base_dir = os.path.abspath('../data')
+	duplicates = find_duplicate_files(base_dir)
+	for dup in duplicates:
+		print(f'\nDuplicate: {dup[0]}\nOriginal: {dup[1]}')
